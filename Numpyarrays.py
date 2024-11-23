8e1971c41f48b47e2960acb99a3a73ac88016048
Godfrey Ng'ang'a <godfreynganga50@gmail.com>
2024-11-23 11:06:48 +0300

    No.of dimension array in a variable

--- Numpyarrays.py
+++ Numpyarrays.py
@@ -11,3 +11,14 @@ print(arr_2d)
 
 arr_3d =([[[1,2,3],[4, 5, 6]], [[1,2,3], [4,5,6]]])
 print(arr_2d)
+
+#checking number of dimensions
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
